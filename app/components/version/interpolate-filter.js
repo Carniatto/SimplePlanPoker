0f6cb3b607fcb69e8d04583f160b9ab4cd8eeb92
Mateus Carniatto <carniatto@TrombaMAC.home>
2015-05-11 19:58:42 -0300

    clean up

--- app/components/version/interpolate-filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ppo.version.interpolate-filter', [])
-
-.filter('interpolate', ['version', function(version) {
-  return function(text) {
-    return String(text).replace(/\%VERSION\%/mg, version);
-  };
-}]);
--- app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ppo.version', [
-  'ppo.version.interpolate-filter',
-  'ppo.version.version-directive'
-])
-
-.value('version', '0.1');
