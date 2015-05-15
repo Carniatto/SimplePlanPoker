6c9f2e0df53c0c99f15c2e85205782feaf000aa9
Mateus Carniatto <mcarniatto@lenovo.com>
2015-05-15 11:40:26 -0300

    Implemented an Directive "Deck"
    This patch makes available <deck /> directive for better visualization
    of the cards when playing.

---
+++ app/scripts/directives/DeckDirective.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('ppo')
+
+.directive('deck', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            selected: '=',
+            cards: '=',
+            closeable: '=',
+            start: '=',
+            selection: '&'
+        },
+        template: ['<div class="pokerCard" ',
+            'ng-class="{selected:(selected && closeable) ',
+            '|| start || selected==card, front:selected==card}"',
+            'ng-repeat="card in cards track by $index" ',
+            'ng-style="getStyle($index)" ',
+            'ng-click="selectCard(card)">',
+            '<div class="ribbon"></div>',
+            '<div class="ribbonValue">{{card}}</div>',
+            '<div class="value">{{card}}</div>',
+            '</div>'
+        ].join(""),
+        link: function(scope, element, attrs) {
+            scope.selectCard = function(value) {
+                if (scope.start) {
+                    scope.start = false;
+                    scope.selected = '';
+                } else {
+                    if (scope.selected === value) {
+                        scope.selected = '';
+                    } else {
+                        scope.selected = value;
+                    }
+                    if (!!attrs.selection) {
+                        var express = scope.selection();
+                        express(value);
+                    }
+                }
+            };
+            scope.getStyle = function(count) {
+                var cards = scope.cards;
+                var rotfactor = 180 / (cards.length - 1);
+                var rotation = rotfactor * (count) - 120;
+                return {
+                    'transform': 'rotate(' + rotation + 'deg)',
+
+                }
+            }
+        }
+    };
+});
